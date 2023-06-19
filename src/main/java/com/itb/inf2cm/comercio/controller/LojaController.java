de65aae08a54fc751c59e13df957c4ed3a1d17b1
rm86489 <rm86489@BF-P4L4-M10.fieb.local>
2023-06-19 09:44:03 -0300

    19/06

--- src/main/java/com/itb/inf2cm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2cm/comercio/controller/LojaController.java
@@ -1,18 +1,47 @@
 package com.itb.inf2cm.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2cm.comercio.model.Produto;
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 	
 	@GetMapping("/listar")
-	public String testProduto() {
-		System.out.println("saida de dados");	
+	public String testProduto(Model model) {
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
+		
+		Produto p1 = new Produto();
+		
+		p1.setId(2l);
+		p1.setNome("televisor samsung");
+		p1.setCodigoBarras("infinito12425");
+		p1.setPreco(100);
+		p1.setDescricao("tv boa utilitaria");
+		
+		Produto p2 = new Produto();
+	p2.setNome("tv nao utilitaria mas boa");
+	 p2.setCodigoBarras("95849294");
+	 p2.setId(12l);
+	 p2.setPreco(150.00);
+	 p2.setDescricao("televisor samsung oi");
+	 
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
+		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
 		return "produtos"; // retorna o nome do template
 	}
-	
-	
+	@GetMapping("/novo-produto")
+	public String adicionarProduto () {
+		
+		return "novo-prod";
+	}
 }
--- src/main/java/com/itb/inf2cm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2cm/comercio/model/Produto.java
@@ -4,11 +4,50 @@ public class Produto {
 
 	private Long id;
 	private String nome;
-	private String codigo8barras;
-	private double preço;
-	
+	private String codigoBarras;
+	private double preco;
+	private String descricao;
 	//public acesso liberado para todas as classes
 	//private: acesso liberado apenas para os membros da propia
 	//         classe, ou seja, só é acessado dentro da página.
+	
+	// Método Setter's -> Utilizamos para atribuir valor ao atributo
+	// Método Getter's -> Utilizamos para "arecuperar" o valor do atributo
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
 }
 
